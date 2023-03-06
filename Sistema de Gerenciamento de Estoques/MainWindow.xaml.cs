77f077fb49d3c5414ba2e4273bb493c4f7251a2c
Victor Guilherme <93740768+1-LEI-DE-NEWTON@users.noreply.github.com>
2023-03-06 18:53:13 -0300

    Forma inicial
    Criação do aplicativo usando WPF, criação da tela de adicionar produtos

---
+++ Sistema de Gerenciamento de Estoques/MainWindow.xaml.cs
@@ -0,0 +1,68 @@
+﻿using System.Windows;
+using Sistema_de_Gerenciamento_de_Estoques;
+
+
+namespace GerenciadorDeEstoque
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private Models models;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            models = new Models();
+        }
+
+        private void AdicionarProduto(object sender, RoutedEventArgs e)
+        {
+            var produto = new Produto(txtNome.Text, int.Parse(txtQuantidade.Text), double.Parse(txtPreco.Text));
+            models.AdicionarProduto(produto);
+            MessageBox.Show("Produto adicionado com sucesso!");
+        }
+
+        private void ListarProdutos(object sender, RoutedEventArgs e)
+        {
+            var produtos = models.ListarProdutos();
+            foreach (var produto in produtos)
+            {
+                MessageBox.Show($"Nome: {produto.Nome} - Quantidade: {produto.Quantidade} - Preço: {produto.Preco}");
+            }
+        }
+
+        private void RemoverProduto(object sender, RoutedEventArgs e)
+        {
+            var produto = new Produto(txtNome.Text, int.Parse(txtQuantidade.Text), double.Parse(txtPreco.Text));
+            models.RemoverProduto(produto);
+            MessageBox.Show("Produto removido com sucesso!");
+        }
+
+        /*
+        private void Button_Click_4(object sender, RoutedEventArgs e)
+        {
+            var produto = new Produto(txtNome.Text, int.Parse(txtQuantidade.Text), double.Parse(txtPreco.Text));
+            models.RemoverProduto(int.Parse(txtIndex.Text));
+            MessageBox.Show("Produto removido com sucesso!");
+        }
+        */
+
+        private void RemoverProduto_Click(object sender, RoutedEventArgs e)
+        {
+            // Obter o índice do produto selecionado na lista
+            int index = lstProdutos.SelectedIndex;
+
+            // Verificar se um produto foi selecionado
+            if (index != -1)
+            {
+                // Remover o produto da lista
+                produtos.RemoveAt(index);
+
+                // Atualizar a exibição da lista na interface do usuário
+                lstProdutos.ItemsSource = produtos;
+            }
+        }
+    }
+}
---
+++ Sistema de Gerenciamento de Estoques/Models.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+
+namespace Sistema_de_Gerenciamento_de_Estoques
+{
+    public class Models
+    {
+        private List<Produto> produtos;
+
+        public Models()
+        {
+            produtos = new List<Produto>();
+        }
+
+        public void AdicionarProduto(Produto produto)
+        {
+            produtos.Add(produto);
+        }
+
+        public List<Produto> ListarProdutos()
+        {
+            return produtos;
+        }
+
+        public void RemoverProduto(Produto produto)
+        {
+            produtos.Remove(produto);
+        }
+
+        public void RemoverProduto(int index)
+        {
+            produtos.RemoveAt(index);
+        }
+
+        public void RemoverProduto(string nome)
+        {
+            var produto = produtos.FirstOrDefault(p => p.Nome == nome);
+            produtos.Remove(produto);
+        }
+    }
+
+    public class Produto
+    {
+        public string Nome { get; set; }
+        public int Quantidade { get; set; }
+        public double Preco { get; set; }
+
+        public Produto(string nome, int quantidade, double preco)
+        {
+            Nome = nome;
+            Quantidade = quantidade;
+            Preco = preco;
+        }
+    }
+}
